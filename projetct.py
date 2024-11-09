81791aad289fcf97e9c9e7717b6793f758babae5
Natan Dev <ns5933988+25@gmail.com>
2024-11-09 18:00:17 -0300

    feat: ao especificar o arquivo, remove o background e converte para png

---
+++ projetct.py
@@ -0,0 +1,23 @@
+from rembg import remove
+from PIL import Image
+import io
+import os
+
+input_path = os.path.join('images', 'black-cat.jpg')
+output_path = os.path.join('result', 'test2-back.png')
+
+try:
+
+    with open(input_path, 'rb') as img_file:
+        img_data = img_file.read()
+
+    result_data = remove(img_data)
+
+    img_no_bg = Image.open(io.BytesIO(result_data))
+    img_no_bg.save(output_path, 'PNG')
+
+    print("Imagem processada e salva")
+
+except Exception as e:
+
+    print(f"Ocorreu um erro ao processar a imagem: {e}")
